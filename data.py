00a2fcc6dc151d9360362afee625a1c36990ee24
PoonamTripathy <76696369+PoonamTripathy@users.noreply.github.com>
2022-02-20 13:20:23 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 lst = [
-    ["link1", "14:00", "14:30"],
-    ["link2", "14:31", "15:00"],
-    ["link3", "15:01", "15:30"]
+    ["https://bit.ly/DataStructC", "9:00", "9:58"],
+    ["https://bit.ly/AeroStructures", "11:10", "12:08"],
+    ["https://zoom.us/j/8143311495?pwd=e-space", "12:10", "13:00"]
+    ["https://bit.ly/engMaterials", "13:50", "14:50"]
 ]
